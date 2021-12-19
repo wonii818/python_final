75980fd7aaebfe1d4f917d5cfe3404c7f22d92b3
2005979 <db5435@naver.com>
2021-12-19 15:47:38 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'z+jyh8_2a+6d5)3u@z7*ak%ke0bwbrfz!z5bupb0&mu4d+^rjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
